7dd3d710fc009fcb9edf9a2b1987b578d10a5f13
Kristian Skibb Skibrek <kristianskibrek@protonmail.com>
2023-12-11 19:57:07 +0100

    initial commit

---
+++ inputOutput/lineCounting.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    int c, nl;
+
+    char randomchar = 'f';
+
+    nl = 0;
+
+    while((c = getchar()) != EOF){
+        if (c == '\n'){
+            ++nl;
+        }
+    }
+
+    printf("%d\n", nl);
+
+}

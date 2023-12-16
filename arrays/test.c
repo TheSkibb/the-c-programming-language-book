6c5cd8cfe3931b1fce87c9dd9e63353cf20aac53
Kristian Skibb Skibrek <kristianskibrek@protonmail.com>
2023-12-16 22:57:07 +0100

    remove old files

--- arrays/test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-/*prints digits and their corresponding int value*/
-int main(){
-    for(int i = 0; i <= 9; i++){
-        printf("%d: %d\n", i, '0' + i);
-    }
-}
--- inputOutput/tasks/task1-11.md
+++
@@ -1,6 +0,0 @@
-To test the wordcounting program, i would test examples like
-
-examin-
-ate
-
-Or similar cases where one word could be broken across two lines
